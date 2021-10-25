2a51fa675391828becebf3789fb4e444d8e93259
thomas <thomasmooney26@gmail.com>
2021-10-25 21:09:40 +0100

    JS updates

--- public/js/script.js
+++ public/js/script.js
@@ -1,6 +1,24 @@
 const form = document.querySelector('form');
 const input = document.querySelector('input');
 
+// not a movie
+const warning = document.createElement('div');
+warning.classList.add('warning');
+
+const text = document.createElement('p');
+text.classList.add('warning-text');
+
+main = document.querySelector('.main');
+warning.appendChild(text);
+
+main.appendChild(warning);
+
+// movie
+const movie = document.createElement('div');
+movie.classList.add('movie')
+main.appendChild(movie);
+
+// add form
 form.addEventListener('submit', (e) => {
   e.preventDefault();
 
@@ -13,49 +31,77 @@ form.addEventListener('submit', (e) => {
       const { Title, Year, imdbID, Director, Writer, Actors, Plot, Ratings, Type  } = data;
       
         if (Type !== "movie"){
-      
           // handle when it's not a movie (tv show for example)
-
-          // create a div
-          const text = document.createElement('p');
-          text.classList.add('warning-text')
           text.textContent = `No movie found with the title of '${search}'. Please search again.`
-          
-          // create a p tag
-          const warning = document.createElement('div');
-          warning.classList.add('warning')
-
-          warning.appendChild(text)
-
-          // add to DOM
-          main = document.querySelector('.main')
-          console.log(main)
-          main.appendChild(warning);
-
         } else {
-        // shouldn't be needed as there is no movie div in the HTML ..should be dynamic
-         const movie = document.querySelector('.movie').style.visibility = "visible";
+              movie.innerHTML = 
+              `<div class="movie_header">
+                <h1 id="title">
+                  <a href="https://www.imdb.com/title/${imdbID}/" target="_blank" rel="noopener noreferrer">${Title}</a>
+                </h1>
+                <p id="year">(${Year})</p>
+               </div>
+        
+            <div class="movie_ratings">
+              <div>
+                <div class="ratings-heading">
+                  <p id="imdb">IMDB</p>
+                </div>
+                <div class="ratings-item">
+                  <p id="rating1">${Ratings[0].Value}</p>
+                </div>
+              </div>
+        
+              <div>
+                <div class="ratings-heading">
+                  <p id="rotten_tomatoes">Rotten Tomatoes</p>
+                </div>
+                <div class="ratings-item">
+                  <p id="rating2">${Ratings[1].Value}</p>
+                </div>
+              </div>
+        
+              <div>
+                <div class="ratings-heading">
+                  <p id="metacritic">Metacrtic</p>
+                </div>
+                <div class="ratings-item">
+                  <p id="rating3">${Ratings[2].Value}</p>
+                </div>
+              </div>
+            </div>
+        
+            <div class="movie_details">
+              <div class="details-heading">
+                <p id="label_director">Director:</p>
+              </div>
+              <div class="detail-item">
+                <p id="director">${Director}</p>
+              </div>
+        
+              <div class="details-heading">
+                <p id="label_writer">Writer(s):</p>
+              </div>
+              <div class="detail-item">
+                <p id="writer">${Writer}</p>
+              </div>
+        
+              <div class="details-heading">
+                <p id="label_actors">Actors:</p>
+              </div>
+              <div class="detail-item">
+                <p id="actors">${Actors}</p>
+              </div>
+        
+              <div class="details-heading">
+                <p id="label_plot">Plot:</p>
+              </div>
+              <div class="detail-item">
+                <p id="plot">${Plot}</p>
+              </div>
+            </div>`
 
-              // create HTML divs etc
-              title.innerHTML = `<a href="https://www.imdb.com/title/${imdbID}/" target="_blank" rel="noopener noreferrer">${Title}</a>`;
-              year.textContent = `(${Year})`;
-              rotten_tomatoes.textContent = "Rotten Tomatoes";
-              imdb.textContent = "IMDB";
-              metacritic.textContent = "Metacrtic";
-
-              rating1.textContent = Ratings[0].Value;
-              rating2.textContent = Ratings[1].Value;
-              rating3.textContent = Ratings[2].Value;
-
-              label_director.textContent = "Director:";
-              label_writer.textContent = "Writer(s):";
-              label_actors.textContent = "Actors:";
-              label_plot.textContent = "Plot:";
-              director.textContent = Director;
-              writer.textContent = Writer;
-              actors.textContent = Actors;
-              plot.textContent = Plot;
-        } 
+            } 
         })
       })
   })
