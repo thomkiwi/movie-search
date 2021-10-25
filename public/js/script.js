5de457b123f29fc43206652d07f1a6b9d8eee299
thomas <thomasmooney26@gmail.com>
2021-10-25 16:24:45 +0100

    handling non movies

--- public/js/script.js
+++ public/js/script.js
@@ -8,12 +8,34 @@ form.addEventListener('submit', (e) => {
 
   fetch('/movies?search=' + search).then((response) => {
     response.json().then((dataObj) => {
-
       const data = dataObj.data;
-      const { Title, Year, imdbID, Director, Writer, Actors, Plot, Ratings  } = data;
 
+      const { Title, Year, imdbID, Director, Writer, Actors, Plot, Ratings, Type  } = data;
+      
+      if (Type !== "movie"){
+        // handle when it's not a movie 
+        title.textContent = `No movie found with title of '${search}'. Please search again.`;
+        title.style.visibility = "visible";
+        year.style.visibility = "hidden";
+
+        rotten_tomatoes.textContent = "Rotten Tomatoes";
+        imdb.textContent = "IMDB";
+        metacritic.textContent = "Metacrtic";
+
+        rating1.textContent = "-";
+        rating2.textContent = "-";
+        rating3.textContent = "-";
+
+        label_director.textContent = "Director:";
+        label_writer.textContent = "Writer(s):";
+        label_actors.textContent = "Actors:";
+        label_plot.textContent = "Plot:";
+        director.textContent = "-";
+        writer.textContent = "-";
+        actors.textContent = "-";
+        plot.textContent = "-";
+      } else {
       const movie = document.querySelector('.movie').style.visibility = "visible";
-
 
             title.innerHTML = `<a href="https://www.imdb.com/title/${imdbID}/" target="_blank" rel="noopener noreferrer">${Title}</a>`;
             year.textContent = `(${Year})`;
@@ -33,7 +55,7 @@ form.addEventListener('submit', (e) => {
             writer.textContent = Writer;
             actors.textContent = Actors;
             plot.textContent = Plot;
-
+      } 
         })
       })
   })
